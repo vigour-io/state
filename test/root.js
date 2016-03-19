1fc97401336ec5aab4a1a8b704b16dcd3d0712fa
Jim de Beer <jim@vigour.io>
2016-03-19 10:59:10 +0100

    root

--- test/root.js
+++ test/root.js
@@ -1,1 +1,24 @@
 'use strict'
+var test = require('tape')
+var s = require('../s')
+var subsTest = require('./test')
+
+test('root subscription', function (t) {
+  var state = s({
+    field: true,
+    something: false
+  })
+
+  var subs = subsTest(t, state, {
+    field: true,
+    other: {
+      yuzi: true,
+      something: {
+        a: true,
+        b: { '~': { james: true } }
+      }
+    }
+  })
+
+  t.end()
+}
