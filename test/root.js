d03d6b8782c6d40b0b85fbbef1847aaee798a865
Jim de Beer <jim@vigour.io>
2016-03-20 11:07:40 +0100

    initial

--- test/root.js
+++ test/root.js
@@ -5,7 +5,7 @@ var subsTest = require('./test')
 test('root subscription', function (t) {
   var subs = subsTest(
     t,
-    { something: true },
+    { something: { b: true } },
     {
       something: {
         a: true,
@@ -14,13 +14,21 @@ test('root subscription', function (t) {
     }
   )
 
+  subs(
+    'initial subscription', [],
+    { something: { $: 1, b: { $: 1 } } }
+  )
+
   // if root (or parent) walk parent tree (wal till root obj)
   // add hasComposite:  //what does it have inside? prob a function
 
   subs(
     'create root field',
-    [ { path: 'james', type: 'new' } ],
-    { something: 1, james: 2 },
+    [ { path: 'b', type: 'update' } ],
+    // just add the number colleciton change is way too small anyways
+
+    // something(1) + james(2)
+    { something: { $: 3, b: { $: 3 } }, james: 2 },
     { james: true }
   )
 
