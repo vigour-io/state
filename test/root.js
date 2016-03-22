91fce8534812a36ca9dc57be3f30f9e60834c2c0
Jim de Beer <jim@vigour.io>
2016-03-22 08:59:07 +0100

    perf tests

--- test/root.js
+++ test/root.js
@@ -4,6 +4,8 @@ var test = require('tape')
 var subscribe = require('../subscribe')
 var s = require('../s')
 
+var Observable = require('vigour-observable')
+
 var Event = require('vigour-event')
 
 test('root subscription', function (t) {
@@ -59,23 +61,42 @@ test('root subscription', function (t) {
   console.log(JSON.stringify(tree, false, 2))
 
   // speed tests
-  console.time('100k updates')
-  var h = state.james.hello
-  // var ev = new Event()
-  for (var i = 0; i < 1e5; i++) {
-    h.set(i)
-  }
-  // tree diff is ery fast 400ms for 100k -- totatly within range (for this hard case 3 tracks updating)
-  // do some tests tmrw with dom as well (prelimmenary start!)
+  // console.time('100k updates')
+  // // var tt = process.hrtime()
+  // var h = state.james.hello
+  // // var ev = new Event()
+  // for (var i = 0; i < 1e5; i++) {
+  //   h.set(i)
+  // }
+  // // tree diff is ery fast 400ms for 100k -- totatly within range (for this hard case 3 tracks updating)
+  // // do some tests tmrw with dom as well (prelimmenary start!)
 
-  // ev.trigger()
-  // subs obj is only 70ms! fix obs
-  // get this faster!
+  // // ev.trigger()
+  // // subs obj is only 70ms! fix obs
+  // // get this faster!
+  // // var tl = process.hrtime(tt)
+  // // will use hrtime for perf tests
+  // // console.log(tl)
+  // console.timeEnd('100k updates')
+
+  var a = new Observable({
+    on: {
+      data () {
+
+      }
+    }
+  })
+
+  console.time('100k updates')
+  for (var i = 0; i < 1e5; i++) {
+    // var ev = new Event()
+    // ev.trigger()
+    a.set(i)
+  }
   console.timeEnd('100k updates')
 
   // diffing is rly good observable is going ultra slow on this!
-  // tree at +/- 60ms
-
+  // tree at +/- 60ms /10k --- obs 220ms -- not acceptable! -- tree should be heaviest part
   // console.log('#update on nested james field')
   t.end()
 })
