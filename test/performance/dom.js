d455042ee0500562f2d0d4ae4034e7ae38ed7412
Jim de Beer <jim@vigour.io>
2016-04-10 19:51:14 +0200

    start of element in state

--- test/performance/dom.js
+++ test/performance/dom.js
@@ -7,26 +7,33 @@ document.body.appendChild(stats.domElement)
 var subscribe = require('../../subscribe')
 var s = require('../../s')
 var state = s({ something: {} })
-var amount = 1e4
-// -------------------------
-function goDom () {
-  stats.begin()
+var amount = 10
 
-  stats.end()
-  window.requestAnimationFrame(goDom)
-}
+var Observable = require('vigour-observable')
+// -------------------------
+var app = new Observable({
+  a: {
+    b: {
+      c: {
+        $: { field: true }
+      }
+    }
+  }
+})
+
 // -------------------------
 function listen (type) {
-
+  console.log('yo yo yo')
 }
 // -------------------------
+// so what we want is a hidden tree??? -- its strange but also makes tons of sense
+
 // make this extra heavy
 var tree = subscribe(
-  state,
+  app,
   { something: { $any: true } },
   listen
 )
 // -------------------------
 console.log('TREE', tree)
 console.log('START ' + amount)
-goDom()
