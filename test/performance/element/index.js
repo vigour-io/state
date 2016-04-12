f0de4972f8707b024339b8afbcf86eb8cfd2ba37
Jim de Beer <jim@vigour.io>
2016-04-12 08:21:26 +0200

    element (speccing)

--- test/performance/element/index.js
+++ test/performance/element/index.js
@@ -9,73 +9,76 @@ var subscribe = require('../../../subscribe')
 var s = require('../../../s')
 var vstamp = require('vigour-stamp')
 console.log(vstamp)
-// var state = s({
-//   name: 'trees'
-// })
-// var obj = {}
-// for (var i = 0; i < 5; i++) {
-//   obj[i] = i
-// }
-// state.set({ collection: obj }, false)
+var state = s({
+  name: 'trees'
+})
+var obj = {}
+for (var i = 0; i < 5; i++) {
+  obj[i] = i
+}
+state.set({ collection: obj }, false)
 // // -------------------------
-// var Element = new Observable({
-//   properties: {
-//     $: true,
-//     $any: true,
-//     text: true,
-//     nodeType: true,
-//     state: true,
-//     _node: true
-//   },
-//   inject: require('../map'), // needs to be very different ofcourse
-//   Child: 'Constructor'
-// }).Constructor
+var Element = new Observable({
+  properties: {
+    $: true, // this basiclly means field
+    // hard thing is to get multiple fields on one level -- we will not support it
+    $any: true,
+    text: new Observable({
+      on: {
+        data (val, stamp) {
+          console.log('this is a text letz go!!!', stamp)
+        }
+      }
+    }),
+    nodeType: true,
+    state: true,
+    _node: true
+  },
+  inject: require('../map'), // needs to be very different ofcourse
+  Child: 'Constructor'
+}).Constructor
+// map is ofcourse wrong
+// subscribe needs to work
 
-// // so we store nodes on the element directly (when its nessecary)
+// wtf to do
+
+var app = new Element({
+  holder: {
+    nested: {
+      collection: {
+        $: 'field',
+        $any: true,
+        Child: {
+          dot: {
+            field: {
+              text: 'hello this is not a subscription'
+            }
+          },
+          text: { $: 'title' }
+        }
+      }
+    },
+    something: {
+      anotherfield: {
+        text: 'more text'
+      },
+      text: 'some text'
+    }
+  }
+})
+
+// so this thing need to get a tree to nested --
+// no not? nope
+/*
+tree: {
 
 
 
-// var app = new Element({
-//   key: 'app'
+}
+*/
 
-// })
 
-// function walk (elem) {
-//   var node = document.createElement(elem.nodeType || 'div')
-//   node.className = elem.key
-//   if (elem._on) {
-//     elem._on.each((p, key) => {
-//       node.addEventListener(key, function (e) {
-//         p.fn.val.call(elem, e)
-//       })
-//     })
-//   }
-//   if (elem.$) { elem._node = node }
-//   elem.each(function (p) {
-//     node.appendChild(walk(p))
-//   })
-//   return node
-// }
-// // isnt it a million times easier to just parse the subs and show that??? this can be very very heavy im affraid
-// var appelem = walk(app)
 
-// /*
-//   few cases
-//   refs and switcher using property defintionw
-//   DONT WANT TO DO TREE FOR NON-CHANGING DOM HANDLES
-// */
-
-// // -------------------------
-// // need to batch it
-// function listen (type, stamp, subs, tree) {
-
-// }
-// // -------------------------
-// var tree = subscribe(state, app.$map(), listen)
-
-// // -------------------------
-// document.body.appendChild(appelem)
 // -------------------------
 console.timeEnd('START')
 global.state = state
-console.log('TREE', tree)
