05ce9949ce7105a75b75adf9ced75fab693c28a9
Jim de Beer <jim@vigour.io>
2016-04-29 13:00:30 +0200

    fixed the multiple subs case

--- test/root/basic.js
+++ test/root/basic.js
@@ -1,28 +1,21 @@
 'use strict'
 const test = require('tape')
+const subsTest = require('../test')
 
 test('root - basic', function (t) {
-  t.plan(2)
-  const State = require('../../lib')
-  const s = new State({
-    key: 'ROOT',
-    a: true
-  })
-  s.subscribe({
+  const subscription = {
     a: {
       $root: {
         b: { val: true }
       }
     }
-  }, function (state, type, stamp, subs, tree) {
-    t.equal(state.path().join('/'), 'ROOT/b', 'correct state')
-    t.equal(type, 'new', 'correct type')
-  })
-  s.set({ b: 'hello b!' })
+  }
+  const s = subsTest(t, { a: true }, subscription)
+  s('set b', [ { path: 'b', type: 'new' } ], false, { b: 'hello b!' })
+  t.end()
 })
 
 test('root - basic - multiple', function (t) {
-  const subsTest = require('../test')
   const subscription = {
     a: {
       $root: {
@@ -32,11 +25,9 @@ test('root - basic - multiple', function (t) {
     }
   }
   const s = subsTest(t, { a: true }, subscription)
-  t.plan(2)
   s('initial subscription', [])
   s('set b', [ { path: 'b', type: 'new' } ], false, { b: 'hello b!' })
   s('set c', [ { path: 'c', type: 'new' } ], false, { c: 'hello c!' })
-  console.log('------------------------- \nupdate c --- here it goes wrong!')
   s('update c', [ { path: 'c', type: 'update' } ], false, { c: 'hello c2!' })
   t.end()
 })
--- test/root/index.js
+++ test/root/index.js
@@ -1,3 +1,4 @@
 'use strict'
 require('./basic')
-// require('./complex')
+require('./complex')
+require('./collection')
