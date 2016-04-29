ea58565510383898c029b47bee7ebd303b4c424e
Jim de Beer <jim@vigour.io>
2016-04-29 13:06:53 +0200

    collection test

--- test/root/collection.js
+++ test/root/collection.js
@@ -1,2 +1,30 @@
 'use strict'
-// const test = require('tape')
+const test = require('tape')
+const subsTest = require('../test')
+
+test('root - collection', function (t) {
+  const subscription = {
+    a: {
+      $any: {
+        $root: {
+          b: { val: true }
+        }
+      }
+    }
+  }
+  const a = [ 1, 2, 3, 4 ]
+  const s = subsTest(t, { a: a, b: 'hello b!' }, subscription)
+  s('initial subscription', multiple('update'))
+
+  // needs to fire 4 times!
+  s('set b', multiple('update'), false, { b: 'hello b2!' })
+  t.end()
+
+  function multiple (type) {
+    const val = []
+    for (let i = 0, len = a.length; i < len; i++) {
+      val.push({ type: type, path: 'b' })
+    }
+    return val
+  }
+})
