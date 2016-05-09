9fdad977ec605012f6cd0f0353405e2d7199a3fe
Jim de Beer <jim@vigour.io>
2016-05-09 10:35:43 +0200

    removed logs

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict'
 require('./basic')
-require('./collection')
-require('./references')
-require('./root')
+// require('./collection')
+// require('./references')
+// require('./root')
 // require('./combined')
 // require('./switch')
