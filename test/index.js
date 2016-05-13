4768db3422cf164f4899e8c0b9ca2e00e20e35ef
Jim de Beer <jim@vigour.io>
2016-05-13 18:27:43 +0200

    enabled all tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict'
-// require('./basic')
-// require('./collection')
-// require('./references')
+require('./basic')
+require('./collection')
+require('./references')
 require('./root')
-// require('./combined')
-// require('./switch')
+require('./combined')
+require('./switch')
