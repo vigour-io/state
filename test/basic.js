024511c6cca6182139b7a32687f025e827edc505
Jim de Beer <jim@vigour.io>
2016-05-15 09:14:41 +0200

    clean up key to number

--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,6 @@
 'use strict'
 const test = require('tape')
+const Observable = require('vigour-observable')
 const subsTest = require('./test')
 
 test('basic', function (t) {
@@ -114,23 +115,12 @@ test('basic - done', function (t) {
   t.end()
 })
 
-test('basic - subscribe method', function (t) {
-  t.plan(3)
-  const s = require('../s')
-  const state = s({ haha: true }, false)
-  state.subscribe({ haha: { val: true } }, function (targetState, type, stamp) {
-    t.equal(targetState, state.haha, 'correct state')
-    t.equal(type, 'new', 'correct type')
-    t.equal(stamp, 0, 'correct stamp')
-  })
-})
-
 test('basic - top', function (t) {
   t.plan(3)
   const s = require('../s')
   const state = s({ haha: true }, false)
   const cnt = { done: 0, new: 0, update: 0 }
-  state.subscribe({ val: true, done: true }, function (targetState, type, stamp, subs, tree, sType) {
+  state.subscribe({ val: true, done: true }, function (target, type, stamp, subs, tree, sType) {
     if (sType) { cnt[sType]++ }
     cnt[type]++
   })
@@ -139,3 +129,24 @@ test('basic - top', function (t) {
   t.equal(cnt.new, 2, 'fired new twice')
   t.equal(cnt.update, 2, 'fired update twice')
 })
+
+test('basic - subscribe method', function (t) {
+  t.plan(5)
+  const s = require('../s')
+  const state = s({ 1: true }, false)
+  const obs = new Observable()
+  state.subscribe({ 1: { val: true } }, function (target, type, stamp) {
+    t.equal(target, state[1], 'correct state')
+    t.equal(type, 'new', 'correct type')
+    t.equal(stamp, 0, 'correct stamp')
+  }, void 0, void 0, obs)
+  t.equal('attach' in state.__on.subscription, true, 'has observable attached')
+  obs.remove()
+  t.equal(
+    state.__on.subscription.attach.keys(),
+    false,
+    'removing observable removes subscription'
+  )
+  state[1].set('hello!')
+})
+
