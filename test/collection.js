1043a474ab19803dd4c2889310a62bdaf8ca9298
Jim de Beer <jim@vigour.io>
2016-05-20 19:33:26 +0200

    fixed a problem in collection (has its own tree now)

--- test/collection.js
+++ test/collection.js
@@ -134,3 +134,36 @@ test('collection - val: "property"', function (t) {
 
   t.end()
 })
+
+test('collection - combined with a field with nested subs', function (t) {
+  var s = subsTest(
+    t,
+    {},
+    {
+      field: { nested: { val: true } },
+      $any: { val: true }
+    }
+  )
+
+  s('initial subscription', [], {})
+
+  // uses same subs -- $ is same and then it goes wrong
+  // 2 options -- if colleciton and setting someone else of the other way arround need to
+  // exclude field from collection or merge
+  s(
+    'create fields',
+    [
+      { path: 'field/nested', type: 'new' },
+      { path: 'a', type: 'new' },
+      { path: 'field', type: 'new' }
+    ],
+    {
+      a: {},
+      field: {
+        nested: 'hello'
+      }
+    }
+  )
+
+  t.end()
+})
