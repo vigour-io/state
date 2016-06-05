a6db24d3a62bb3c68ad4713833a1228f16ba2aec
Jim de Beer <jim@vigour.io>
2016-06-05 22:01:39 +0200

    fixed siwthcing refrences for collection

--- test/references/collection.js
+++ test/references/collection.js
@@ -1,6 +1,7 @@
 'use strict'
 const test = require('tape')
 const subsTest = require('../util')
+const State = require('../../lib')
 
 test('reference - collection - fields', function (t) {
   const s = subsTest(
@@ -68,7 +69,81 @@ test('reference - collection - target - struct', function (t) {
     return val
   }
 
-  console.log('what ze fuck')
+  s(
+    'remove reference',
+     multiple('remove', true),
+     { a: false }
+   )
+  t.end()
+})
+
+test('reference - collection - over reference', function (t) {
+  const state = new State({
+    a: {
+      a1: true,
+      a2: true
+    },
+    b: {
+      b1: true
+    },
+    collection: '$root.a'
+  })
+
+  const s = subsTest(
+    t,
+    state,
+    {
+      collection: {
+        $any: { val: true }
+      }
+    }
+  )
+  s(
+    'initial subscription',
+    [
+      { path: 'a/a1', type: 'new' },
+      { path: 'a/a2', type: 'new' }
+    ]
+  )
+  s(
+    'update 0',
+    [
+      { type: 'remove' },
+      { type: 'remove' },
+      { path: 'b/b1', type: 'new' }
+    ],
+    {
+      collection: '$root.b'
+    }
+  )
+  t.end()
+})
+
+test('reference - collection - target - struct', function (t) {
+  const subscription = {
+    a: {
+      $remove: true,
+      $any: {
+        $remove: true,
+        title: { val: true }
+      }
+    }
+  }
+  const b = [ { title: 1 }, { title: 2 }, { title: 3 }, { title: 4 } ]
+  const s = subsTest(t, { b: b, a: '$root.b' }, subscription)
+  s('initial subscription', multiple('new'))
+  function multiple (type, nopath) {
+    const val = []
+    for (let i = 0, len = b.length; i < len; i++) {
+      if (nopath) {
+        val.push({ type: type })
+      } else {
+        val.push({ type: type, path: 'b/' + i + '/title' })
+      }
+    }
+    return val
+  }
+
   s(
     'remove reference',
      multiple('remove', true),
