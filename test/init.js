cb1e09b068312adfebce757a30d0275024e6b434
Jim de Beer <jim@vigour.io>
2016-07-05 19:08:18 +0200

    added test

--- test/init.js
+++ test/init.js
@@ -1,9 +1,16 @@
 'use strict'
 const test = require('tape')
 const vstamp = require('vigour-stamp')
+const State = require('../')
 
 test('init', function (t) {
-  const State = require('../') // eslint-disable-line
+  const state = new State({}, false)
+  t.equal(state.isState, true)
+  t.equal(vstamp.cnt, 0, 'vstamp.cnt is zero')
+  t.end()
+})
+
+test('async helpers', function (t) {
   const state = new State({}, false)
   t.equal(state.isState, true)
   t.equal(vstamp.cnt, 0, 'vstamp.cnt is zero')
