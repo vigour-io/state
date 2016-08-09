cff3f0533d00351e55a5e6d9e7507c55ee927503
Jim de Beer <jim@vigour.io>
2016-08-09 17:59:22 +0200

    back to 100% coverage

--- test/resubscribe.js
+++ test/resubscribe.js
@@ -29,10 +29,15 @@ test('resubscribe', function (t) {
   }, (state) => {
     cnt++
   })
+  instance.subscribe({
+    a: { val: true }
+  }, (state) => {
+    cnt++
+  }, void 0, void 0, 'attach')
   cnt = 0
   original = 0
   instance.resubscribe()
-  t.equal(cnt, 1, 'refire subscription')
+  t.equal(cnt, 2, 'refire subscription')
   t.equal(original, 0, 'dont refire original subscription')
   t.end()
 })
