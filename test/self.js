1bed8632a4dac32f836848b2267fbf4f3e9471bb
Jim de Beer <jim@vigour.io>
2016-09-24 15:06:03 +0200

    test for $self

---
+++ test/self.js
@@ -0,0 +1,40 @@
+'use strict'
+const test = require('tape')
+const subsTest = require('./util')
+
+test('self', function (t) {
+  const s = subsTest(
+    t,
+    {
+      a: {
+        field: 'not so lullz'
+      },
+      field: {
+        val: '$root.a',
+        field: 'lullz'
+      }
+    },
+    {
+      field: {
+        $self: {
+          field: {
+            val: true
+          }
+        }
+      }
+    }
+  )
+
+  s(
+    'initial subscription',
+    [{ path: 'field', type: 'new' }]
+  )
+
+  // s(
+  //   'update nested field',
+  //   [ { path: 'other/yuzi', type: 'new' } ],
+  //   { other: { yuzi: true } }
+  // )
+
+  t.end()
+})
