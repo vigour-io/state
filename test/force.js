93b7035410e75702585e60681d2fbd4da04fa6ae
Jim de Beer <jim@vigour.io>
2016-10-03 15:00:29 +0200

    added tests for any, root, switch, test

--- test/force.js
+++ test/force.js
@@ -2,10 +2,7 @@
 const test = require('tape')
 const State = require('../')
 
-test('force - basic', (t) => {
-  // ADD ROOT SUBSCRIPTION
-  // ADD SWITCH
-
+test('force - basic', t => {
   const state = new State({
     a: {
       b: {
@@ -24,7 +21,6 @@ test('force - basic', (t) => {
     }
   })
   const arr = []
-  // make it with switch instant
   state.subscribe({
     a: {
       b: {
@@ -37,13 +33,10 @@ test('force - basic', (t) => {
               }
             }
           }
-          // test with nested
         }
       }
     }
-  }, state => {
-    arr.push(state.path())
-  })
+  }, state => arr.push(state.path()))
 
   var forceObj = {
     [state.a.sid()]: true, // option to add stamp when nessecary -- maybe clean
@@ -55,12 +48,85 @@ test('force - basic', (t) => {
     [state.b.c.d.sid()]: true,
     [state.b.c.d.e.sid()]: true
   }
+
   state.emit('subscription', forceObj)
+
   t.same(arr, [
     [ 'b', 'title' ],
     [ 'b', 'c', 'd', 'e' ],
     [ 'b', 'title' ],
     [ 'b', 'c', 'd', 'e' ]
-  ])
+  ], 'forces correct updates')
+
+  t.end()
+})
+
+test('force - switch, root and test', t => {
+  var arr = []
+
+  const state = new State({
+    b: {
+      title: 'it\'s b',
+      c: { d: 'hello' }
+    },
+    items: [
+      { title: '#1', description: 'its #1' },
+      { title: '#2', description: 'its #2' }
+    ],
+    field: '$root.b'
+  })
+
+  const subs = {
+    field: {
+      $switch: {
+        exec: state => state.key,
+        b: {
+          val: 1,
+          title: { val: true },
+          c: { d: { val: true } },
+          $root: {
+            items: {
+              $any: {
+                $test: {
+                  exec: val => true,
+                  $: {
+                    title: { val: true }
+                  },
+                  $pass: {
+                    title: { val: true },
+                    description: { val: true }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  state.subscribe(subs, state => arr.push(state.path()))
+  var forceObj = {
+    [state.items.sid()]: true,
+    [state.items[0].sid()]: true,
+    [state.items[1].sid()]: true,
+    [state.items[0].title.sid()]: true,
+    [state.items[1].title.sid()]: true,
+    [state.items[0].description.sid()]: true,
+    [state.items[1].description.sid()]: true
+  }
+  arr = []
+  state.emit('subscription', forceObj)
+
+  t.same(
+    arr,
+    [
+      [ 'items', '0', 'title' ],
+      [ 'items', '0', 'description' ],
+      [ 'items', '1', 'title' ],
+      [ 'items', '1', 'description' ]
+    ], 'refires collection over switch, root and test'
+  )
+
   t.end()
 })
